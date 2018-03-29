2d9dbe1445981a5e791c231302a965bd0e722ad1
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2018-03-28 20:27:17 -0400

    Update test_itensor.py

--- tests/test_itensor.py
+++ tests/test_itensor.py
@@ -1,5 +1,6 @@
 import unittest
 import pitensor
+import math
 
 class PiTensorTest_ITensor(unittest.TestCase):
     def test(self):
@@ -8,6 +9,8 @@ class PiTensorTest_ITensor(unittest.TestCase):
 
     def testIndex(self):
         i = pitensor.Index("I", 3)
+        s = str(i)
+        self.assertTrue(s.startswith('(I,3'))
 
     def testITensor(self):
         i = pitensor.Index("I", 3)
@@ -18,6 +21,9 @@ class PiTensorTest_ITensor(unittest.TestCase):
         S = pitensor.ITensor()
         V = pitensor.ITensor()
         foo = pitensor.svd(T, U, S, V)
+
+        self.assertTrue(math.isclose(pitensor.norm(T), math.sqrt(12.0), rel_tol=1E-6))
+        self.assertTrue(math.isclose(pitensor.norm(S), math.sqrt(12.0), rel_tol=1E-6))
 
 if __name__=='__main__':
     unittest.main()
