7ba167a0b734e16cb750cd3155a17d31395e5214
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2018-04-09 23:22:59 -0400

    Update eigensolver.h.cc: rename init to _init

--- src/itensor/eigensolver.h.cc
+++ src/itensor/eigensolver.h.cc
@@ -9,7 +9,7 @@ using namespace itensor;
 template <typename BigMatrixT, typename Tensor>
 static inline
 auto
-init(pybind11::module& module)
+_init(pybind11::module& module)
 {
   module
       .def("davidson",
@@ -20,5 +20,8 @@ init(pybind11::module& module)
 
 void pitensor::eigensolver(pybind11::module& module)
 {
-
+  _init<LocalMPO<ITensor>, ITensor>(module);
+  _init<LocalMPO<IQTensor>, IQTensor>(module);
+  // TODO: cross-combination?
+  // TODO: BigMatrixT type?
 }
